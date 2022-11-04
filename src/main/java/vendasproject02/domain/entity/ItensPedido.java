63fa8c4d61c026e96de310e769993e165dd081df
unknown <Rubenia>
2022-11-04 11:29:48 -0300

    Documentando com Swagger

--- src/main/java/vendasproject02/domain/entity/ItensPedido.java
+++ src/main/java/vendasproject02/domain/entity/ItensPedido.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 @Table(name = "itens_pedido")
 public class ItensPedido {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @ManyToOne
